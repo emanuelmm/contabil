b5068bad4ee4544c89985544d69197747fefb33c
Emanuel Cordeiro <emanuelcordeiro@gmail.com>
2015-06-28 15:45:35 -0300

    dev

--- src/main/java/br/com/emanuelmm/contab/bean/BalancoBean.java
+++ src/main/java/br/com/emanuelmm/contab/bean/BalancoBean.java
@@ -1,5 +1,7 @@
 package br.com.emanuelmm.contab.bean;
 
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
@@ -11,7 +13,13 @@ public class BalancoBean {
 
 	private Balanco balanco;
 	
+	private List < Balanco > balancos;
+		
 	public void cadastrar() {
+		
+	}
+	
+	public void remover() {
 		
 	}
 
@@ -22,8 +30,13 @@ public class BalancoBean {
 	public void setBalanco(Balanco balanco) {
 		this.balanco = balanco;
 	}
-	
-	
-	
+
+	public List<Balanco> getBalancos() {
+		return balancos;
+	}
+
+	public void setBalancos(List<Balanco> balancos) {
+		this.balancos = balancos;
+	}
 		
 }
